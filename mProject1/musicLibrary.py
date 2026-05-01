600ad129de3fa00ea08116eb08bdbc623f92de20
Harshit252006 <gamingbeastyt2507@gmail.com>
2026-05-01 20:55:29 +0530

    commit

---
+++ mProject1/musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "bairan": "https://youtu.be/u5e4nsM-ql8?si=2M_bnabgwB6m-54b",
+    "kitab": "https://youtu.be/1uypbHj8-Mk?si=rHVQxnYBXrciPbyL",
+    "haayere": "https://youtu.be/gJGcC3hlRcE?si=1sHJyzFMCABdEbrA",
+    "samjhe": "https://youtu.be/qdNeSV5UemY?si=AXwudjs_XZaSAkZq"
+}
